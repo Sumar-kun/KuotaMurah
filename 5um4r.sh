92f75b341b77ee1be239dc05769a49979ddbc824
Sumar-kun <49847531+Sumar-kun@users.noreply.github.com>
2019-04-22 07:32:15 +0700

    Add files via upload

---
+++ 5um4r.sh
@@ -0,0 +1,107 @@
+#!/system/xbin/bash 
+#Maafkan Kegabutan Ku Ster :V
+#bwahaha
+
+blue='\033[34;1m'
+green='\033[32;1m'                                        
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'                                           
+yellow='\033[33;1m'
+
+echo $red "Loading."
+sleep 1
+clear
+echo $blue "Loading.."
+sleep 1
+clear
+echo $green "Loading..."
+sleep 1
+clear
+echo $red "Loading."
+sleep 
+clear
+echo $blue "Loading.."
+sleep 1
+clear
+echo $green "Loading..."
+sleep 1
+clear
+echo $red "wellcome to my tools"
+sleep 1
+clear
+echo $blue "Ilegal Tools"
+sleep 1
+clear
+echo $green "ENJOYYY"
+sleep 1
+clear
+
+echo "\033[1;31m]
+           00000000000000000
+           00000    Mr.c0p4s
+           00000
+           00000000000000000
+                       00000
+           by:sumar    00000
+           00000000000000000
+       +======================+
+       + author : SumarKun    +
+       + kontak : 089561***   +
+       +    NEVER GIVE UP     +
+       +======================+
+      ××××××××××××××××××××××××××××     
+      \ KoutaMurah(AllOperator) / 
+       \_/                   \_/   ";
+echo $green"Menu Kuota Murah";
+echo $blue"1.5GB (Rp.15.000)";
+echo $purple"2.10GB (Rp.25.000)";
+echo $yellow"3.20GB (Rp.40.000)";
+echo $green"4.35GB (Rp.50.000)"
+echo $red"00.keluar"
+read -p "[>>" stc
+
+if [ $stc = 1 ]
+then 
+clear
+echo "MasukanNomerMu"|lolcat
+read no
+echo "Wait 5menit"|lolcat
+echo "$no, Sukses Gan"
+fi
+
+if [ $stc = 2 ]
+then 
+clear
+echo "MasukanNomerMu"|lolcat
+read no
+echo "Wait 5menit"|lolcat
+echo "$no, Sukses Gan"|lolcat
+fi
+
+if [ $stc = 3 ]
+then 
+clear
+echo "MasukanNomerMu"|lolcat
+read no
+echo "Wait 5menit"|lolcat
+echo "$no, Sukses Gan"|lolcat
+fi
+
+if [ $stc = 4 ]
+then 
+clear
+echo "MasukanNomerMu"|lolcat
+read no
+echo "Wait 5menit"|lolcat
+echo "$no, Sukses Gan"|lolcat
+fi
+
+if [ $stc = 00 ]
+then 
+clear
+echo "Apakah Ragil Gans?"|lolcat
+read ganss
+echo "$ganss, Tq Gan"|lolcat
+fi
